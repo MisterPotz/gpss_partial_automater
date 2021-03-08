f77842644bd5fadeb447051a698208f8b5961d92
Alexander Gornostaev <algor@pop-os.localdomain>
2021-03-08 20:40:35 +0300

    generate all necessary data

--- python_csv_fetcher.py
+++ python_csv_fetcher.py
@@ -14,6 +14,7 @@ print(reports_folder.name)
 print([x for x in reports_folder.iterdir()])
 
 data = pd.DataFrame()
+generated_files_list = []
 
 for index, i in enumerate(reports_folder.iterdir()):
     with open(i, 'r') as report:
@@ -30,7 +31,9 @@ for index, i in enumerate(reports_folder.iterdir()):
 
 print(data)
 
-data.to_csv("raw_report.csv", index=False, sep=",")
+raw_report_name = "raw_report.csv"
+data.to_csv(raw_report_name, index=False, sep=",")
+generated_files_list.append(raw_report_name)
 
 import numpy as np
 
@@ -56,7 +59,10 @@ for i in raw_columns:
     print(f"row for stanok {i} : {row}")
     means_disps = means_disps.append(dict(zip(means_disps.columns, row)), ignore_index = True)
 means_disps.index = raw_columns
-means_disps.to_csv("means_and_disps.csv", sep=",", index=True)
+means_and_disps_name = "means_and_disps.csv"
+means_disps.to_csv(means_and_disps_name, sep=",", index=True)
+
+generated_files_list.append(means_and_disps_name)
 
 # confidence tables
 conf = pd.read_csv("confidence_interval.csv", index_col=0)
@@ -64,20 +70,40 @@ conf = pd.read_csv("confidence_interval.csv", index_col=0)
 def find_t(confidence_table, N, accuracy):
     acc = accuracy / 2
     v = N - 1
-    return confidence_table.loc[ confidence_table.index == v, confidence_table.columns.astype('float64') == acc ]
+    t = confidence_table.loc[ confidence_table.index == v, confidence_table.columns.astype('float64') == acc ].to_numpy()[0][0]
+    return t
 
+import math
 def confidence_interval(accuracy, arr):
     mean, disp = mean_disp(arr)
     t = find_t(conf, arr.shape[0], accuracy)
-    diff = t * sqrt(disp / arr.shape[0])
+    diff = t * math.sqrt(disp / arr.shape[0])
     return (mean, diff)
 
+def prettify_confidence_interval(mean, disp):
+    return f"{mean} +- {disp}"
+
+print(conf.columns)
 for i in raw_columns:
     framei = pd.DataFrame(columns=["N = 5", "N = 20"])
     columns = framei.columns
-    new_row = []
-    tetas = list(map(lambda x: confidence_interval(accuracy=), zip([])))
-    framei.append()
-    for accs in [0.1, 0.05]:
-        
-    
+    def create_row(column, accuracy):
+        arr = raw_data[column]
+        row = []
+        for i in [5, 20]:
+            teta = confidence_interval(accuracy, arr[:i])
+            teta = prettify_confidence_interval(teta[0], teta[1])
+            row.append(teta)
+        row = dict(zip(columns, row))
+        return row
+
+    for acc in (0.1, 0.05):
+        row = create_row(i, acc)
+        framei = framei.append(row, ignore_index=True)
+
+    framei.index = ['acc 0.1', 'acc 0.05']
+    framei_name = f"stanok_conf_{i}.csv"
+    framei.to_csv(framei_name, sep=',', index=True)
+    generated_files_list.append(framei_name)
+
+print(f"output files : {generated_files_list}")
